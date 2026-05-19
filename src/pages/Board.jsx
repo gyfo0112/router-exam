a77f2551464b6c8509879588244912c474010ebd
12 <gyfo6474@naver.com>
2026-05-19 16:11:53 +0900

    오타수정

--- src/pages/Board.jsx
+++ src/pages/Board.jsx
@@ -1,30 +1,23 @@
-import React from 'react'
-import { communityPosts } from '../data/board'
-import "./styles/board.css"
+import React from "react";
+import { communityPosts } from "../data/board";
+import "./styles/Board.css";
 const Board = () => {
   return (
-    <section className='board'>
+    <section className="board">
       <div className="inner">
-
         <h2>Community Board</h2>
         <ul className="board-list">
           {communityPosts.map(({ id, title, content }) => (
             <li key={id}>
-              <span>
-                {id}
-              </span>
-              <h4>
-                {title}
-              </h4>
-              <p>
-               {content}
-              </p>
+              <span>{id}</span>
+              <h4>{title}</h4>
+              <p>{content}</p>
             </li>
           ))}
         </ul>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Board
+export default Board;
